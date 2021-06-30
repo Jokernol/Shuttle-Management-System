6faaf4da095f9c09a444bb8d651f8fc0e4e2dcb5
Jokernol <zjh815010623@gmail.com>
2021-06-30 19:50:06 +0800

    add mybatis

---
+++ src/main/java/se/zust/badgateway/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package se.zust.badgateway.mapper;
+
+/**
+ * @author zhu
+ */
+public interface UserMapper {
+}
---
+++ src/main/java/se/zust/badgateway/pojo/user.java
@@ -0,0 +1,12 @@
+package se.zust.badgateway.pojo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class user {
+    String name;
+}
