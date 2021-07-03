742a8e86e578a8a3fbe98a9449a5d176db918120
redamancy-w <w2587892294@163.com>
2021-07-03 12:56:22 +0800

    歇逼了歇逼了歇逼了歇逼了真歇逼了

--- src/main/java/se/zust/badgateway/controller/StationServlet.java
+++ src/main/java/se/zust/badgateway/controller/StationServlet.java
@@ -5,6 +5,7 @@ import se.zust.badgateway.pojo.DO.StationDO;
 import se.zust.badgateway.service.StationService;
 import se.zust.badgateway.util.BeanUtils;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,27 +19,19 @@ import java.util.List;
  */
 @WebServlet("stations/*")
 public class StationServlet extends BaseServlet {
-    /**
-     * 获得全部站点
-     */
-    protected void get(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String url = req.getServletPath();
-        String eUrl = url.substring(url.lastIndexOf("/") + 1);
-        StationService stationService1 = new StationService();
-        List<StationDO> stationList = stationService1.allStation();
-        HttpSession session = req.getSession();
-        session.setAttribute("stationList", stationList);
-        req.getRequestDispatcher("/UserHome.jsp").forward(req, resp);
-    }
 
     /**
      * 添加站点
      */
     protected void post(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         StationDO stationPo = BeanUtils.Request2Bean(req, StationDO.class);
-        StationService stationService = new StationService();
-        System.out.println("ssssss" + stationPo.toString());
-        boolean i = stationService.addStation(stationPo);
+
+        StationService.getInstance().addStation(stationPo);
+
+        ServletContext servletContext = req.getServletContext();
+
+        servletContext.setAttribute("stationList",StationService.getInstance().allStation() );
+
         req.getRequestDispatcher("addSation.jsp").forward(req, resp);
     }
 }
